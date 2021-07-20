4973571a99b4f033caef588bf6c63eecad081b39
Issa Taofeek <taofeeq.issa076@gmail.com>
2021-07-20 20:01:00 +0100

    minor change

--- globe_bank/private/shared/public_footer.php
+++ globe_bank/private/shared/public_footer.php
@@ -2,7 +2,8 @@
     <p>Copyright <?php echo date('Y'); ?>, Globe Bank International </p>
   </footer>
 
-  <p>This is a fictitious company created by <a href="http://linkedin.com">LinkedIn Corporation, or its affiliates</a>, solely for the creation and development of educational training materials. Any resemblance to real products or services is purely coincidental. Information provided about the products or services is also fictitious and should not be construed as representative of actual products or services on the market in a similar product or service category.</p>
+  <!-- <p>This is a fictitious company created by <a href="http://linkedin.com">LinkedIn Corporation, or its affiliates</a>, solely for the creation and development of educational training materials. Any resemblance to real products or services is purely coincidental. Information provided about the products or services is also fictitious and should not be construed as representative of actual products or services on the market in a similar product or service category.</p> -->
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, consequatur eveniet culpa consequuntur modi autem, vitae tenetur nulla rem ab facere atque harum nesciunt deleniti alias sed exercitationem quam quo!</p>
 
   </body>
 </html>
